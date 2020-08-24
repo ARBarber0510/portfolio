981bb8411a3b40299ef0b4fbc865e394c6d29c64
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-24 17:02:33 -0500

    modifying portfolio css layout and js file.

---
+++ script.js
@@ -0,0 +1,7 @@
+var mainContentDiv = $("<div>");
+mainContentDiv.attr("class", "card")
+var mainContentBody = $("<div>");
+mainContentBody.attr("class", "card-body");
+
+
+$("#main-content-div").append()
