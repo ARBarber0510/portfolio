86cc94e3187c5e34035976c4c7f6903b07666143
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-25 17:51:13 -0500

    modifed contact page and email me section.

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-var main = document.querySelector("main");
-var sitesEl = document.querySelector(".sites");
-var site1El = document.querySelector(".site1");
-var site2El = document.querySelector(".site2");
-var site3El = document.querySelector(".site3");
-var siteTitles = document.querySelectorAll("h4");
+// Email form
 
-var sites = ["https://www.linkedin.com/in/alison-barber-b6b45444/", "https://www.instagram.com/alibarbershop/", "https://twitter.com/AliBarbershop"];
+var submitBtn = document.querySelector("#submit-btn");
+var nameInput = document.querySelector("#name-input");
+var emailInput = document.querySelector("#email-input");
+var response = document.querySelector("#response");
 
-site1El.children[0].textContent = "Linkedin";
-site1El.children[1].setAttribute("href", "https://www.linkedin.com/in/alison-barber-b6b45444/");
-site1El.children[1].children[0].setAttribute("alt", "linkedin");
-site1El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-site2El.children[0].textContent = "Instagram";
-site2El.children[1].setAttribute("href", "https://www.instagram.com/alibarbershop/");
-site2El.children[1].children[0].setAttribute("alt", "instagram");
-site2El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-site3El.children[0].textContent = "Twitter";
-site3El.children[1].setAttribute("href", "https://twitter.com/AliBarbershop");
-site3El.children[1].children[0].setAttribute("alt", "twitter");
-site3El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-for (var i = 0; i < siteTitles.length; i++) {
-  siteTitles[i].setAttribute("style", "text-decoration: underline; padding-left:10px; margin: 0");
-}
-
+submitBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
+  submissionResponseEl.textContent = response;
+});
