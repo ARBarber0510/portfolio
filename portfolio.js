856b4540668f5ed6c7754dbd767b6289ae1f5e2f
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-25 16:20:06 -0500

    updated footer

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+    
+//     // Build div for project info
+//     var projectInfoDiv = $("<div>");
+//     projectInfoDiv.attr("class", "card-main-content");
+//     var projectInfoBody = $("<div>");
+//     projectInfoBody.attr("class", "card-body");
+//     var projectCardType = $("<div>");
+//     projectCardType.attr("class", "card bg-info text-white");
+// }
